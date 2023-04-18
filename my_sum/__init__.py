809cd769334b01ee06b94aaf1c103f80e02fb193
Mike DiLalo <mdilalo@rutgers.edu>
2023-04-18 15:19:54 -0400

    Add sum and tests

---
+++ my_sum/__init__.py
@@ -0,0 +1,11 @@
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
+
+def dumbsum(arg):
+    total = 1
+    for val in arg:
+        total += val
+    return total
